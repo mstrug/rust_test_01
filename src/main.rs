2feb9ce8a5219257642b72264edd88b973adede2
michal <michal@localhost>
2022-01-21 01:24:33 +0100

    Writing cpu load to a file.

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,24 @@
-
-use std::{thread, time};
+use std::{thread, time, path, fs, io::Write};
 use systemstat::{System, Platform};
 use chrono::Utc;
 
 
-
-fn main()
+fn run_measurements( file: &mut fs::File )
 {
 	let system = System::new();
-
-	println!("Measurement started...");
-
 	loop 
 	{
 		match system.cpu_load()
 		{
 			Ok(dm) => {
-				println!("timestamp: {}:", Utc::now());
+				writeln!(file, "\ntimestamp: {}:", Utc::now()).unwrap();
 				thread::sleep(time::Duration::from_secs(5));
 				match dm.done()
 				{
 					Ok(v) => {
 						for (i, d) in v.iter().enumerate() {
-						    println!("CPU#{}\n\tUser time: {:.2}%\n\tNice time: {:.2}%\n\tSystem time: {:.2}%\n\tIdle time: {:.2}%", i, d.user*100.0, d.nice*100.0, d.system*100.0, d.idle*100.0);
+							writeln!(file, "CPU#{}\n\tUser time: {:.2}%\n\tInterrupt time: {:.2}%\n\tSystem time: {:.2}%\n\tIdle time: {:.2}%", 
+								i, d.user*100.0, d.interrupt*100.0, d.system*100.0, d.idle*100.0).unwrap();
 						}
 					},
 					Err(x) => {
@@ -34,6 +30,23 @@ fn main()
 				println!("Error getting cpu load: {}", x); return;
 			}
 		};
-		println!("");
 	}	
+}
+
+fn main()
+{
+	let fpath = path::Path::new("/tmp/cpu_load.log");
+
+	println!("Measurement started...");
+	
+	match fs::File::create( &fpath ) {
+		Ok(mut file) => {
+			println!("Writing to a file: {}", fpath.display());
+			run_measurements(&mut file);
+		},
+		Err(x) => {
+			println!("Error creating file: {}, {}", fpath.display(), x);
+			return;
+		}
+	};	
 }
